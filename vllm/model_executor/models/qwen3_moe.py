3a8756298a1f3ddab1ffff1852ada3f2ac3dde35
f00824209 <fuzhihong4@huawei.com>
2026-02-03 18:51:46 +0800

    [doc] adapter fuse op triton_split_qkv_rmsnorm_mrope

--- vllm/model_executor/models/qwen3_moe.py
+++ vllm/model_executor/models/qwen3_moe.py
@@ -298,16 +298,18 @@ class Qwen3MoeAttention(nn.Module):
         hidden_states: torch.Tensor,
     ) -> torch.Tensor:
         qkv, _ = self.qkv_proj(hidden_states)
-        q, k, v = qkv.split([self.q_size, self.kv_size, self.kv_size], dim=-1)
-        # Add qk-norm
-        q_by_head = q.view(*q.shape[:-1], q.shape[-1] // self.head_dim, self.head_dim)
-        q_by_head = self.q_norm(q_by_head)
-        q = q_by_head.view(q.shape)
-
-        k_by_head = k.view(*k.shape[:-1], k.shape[-1] // self.head_dim, self.head_dim)
-        k_by_head = self.k_norm(k_by_head)
-        k = k_by_head.view(k.shape)
-        q, k = self.rotary_emb(positions, q, k)
+        cos_sin = self.rotary_emb.cos_sin_cache[positions]
+        q, k, v = torch.ops.vllm.triton_split_qkv_rmsnorm_mrope(
+            qkv=qkv,
+            q_weight=self.q_norm.weight,
+            k_weight=self.k_norm.weight,
+            cos_sin=cos_sin,
+            num_q_heads=self.num_heads,
+            num_kv_heads=self.num_kv_heads,
+            head_size=self.head_dim,
+            eps=self.q_norm.variance_epsilon,
+            mrope_section=self.rotary_emb.mrope_section,
+        )
         attn_output = self.attn(q, k, v)
         output, _ = self.o_proj(attn_output)
         return output
